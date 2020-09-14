cf93e9177417268848d83fb086bbd5633f603cab
Adil <aughr063@uottawa.ca>
2020-09-14 14:42:23 -0400

    deploy static react frontend

---
+++ build/precache-manifest.6b22e6de159f06d48393a7f65cab7a56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "026c867f30eda1a857f2a0179bbe2c11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b966c84b9c8f47181ab7",
+    "url": "/static/css/main.4d7d9c30.chunk.css"
+  },
+  {
+    "revision": "9af68b89fbfe292cb182",
+    "url": "/static/js/2.c7d8749e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c7d8749e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b966c84b9c8f47181ab7",
+    "url": "/static/js/main.1639afb3.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
--- index.js
+++ index.js
@@ -39,6 +39,7 @@ morgan.token('body', function getBody (req) {
   })
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(bodyParser.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
